1309210cb5a2448ca46b455aca4f3ef8b67e58ba
Windows 10 Complete <Windows 10 Complete>
2015-12-29 20:08:25 +0700

    add href redirect to home page(adminRoute)

--- public/template/backend/html/nav-left.php
+++ public/template/backend/html/nav-left.php
@@ -1,9 +1,9 @@
  <!-- Logo -->
-<a href="#" class="logo">
+<a href="<?php echo $this->url("adminRoute") ?>" class="logo">
   <!-- mini logo for sidebar mini 50x50 pixels -->
   <span class="logo-mini"><b>A</b>LT</span>
   <!-- logo for regular state and mobile devices -->
-  <span class="logo-lg"><b>Admin</b>LTE</span>
+  <span class="logo-lg"><b>Book</b>Store</span>
 </a>
   
                 
